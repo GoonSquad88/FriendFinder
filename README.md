5b6065e1ec32b270701fbfe3c7025fdda172da1c
Jonathan Viera <go_chunkymonkey@yahoo.com>
2019-04-19 18:58:49 -0700

    finished app

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # FriendFinder
+
+Fill out the survey by pressing the "Go to Survey" buttom.
+
+Fill out the survey by starting with your name and a link to your picture.
+
+When you are done, click on submit and a modal will pop up with you best match. Simple! :)
---
+++ app/data/friends.js
@@ -0,0 +1,36 @@
+var friends = [
+    {
+        name: "Ahmed",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores:[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        name: "John",
+        photo: "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjBtIqU99rhAhVPqJ4KHTTEALwQjRx6BAgBEAU&url=https%3A%2F%2Fwww.visordown.com%2Freviews%2Ffirst-ride%2Ffirst-ride-ducati-panigale-v4-s-review-2&psig=AOvVaw0CatscWJQSd9lUzkOc5Wad&ust=1555721156795702",
+        scores: [
+            4,
+            2,
+            3,
+            4,
+            5,
+            1,
+            2,
+            1,
+            5,
+            3
+        ]
+    }
+];
+
+module.exports = friends;
